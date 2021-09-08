9169e7b71c890a53ce4149c9a55341fffb4a7da6
Archit1468 <65843341+Archit1468@users.noreply.github.com>
2021-09-08 10:25:32 +0530

    Update Tableau Project SQL Queries.sql

--- Tableau Project SQL Queries.sql
+++ Tableau Project SQL Queries.sql
@@ -8,7 +8,6 @@ Queries used for Tableau Project
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 where continent is not null 
 --Group By date
 order by 1,2
@@ -19,7 +18,6 @@ order by 1,2
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 --From PortfolioProject..CovidDeaths
-----Where location like '%states%'
 --where location = 'World'
 ----Group By date
 --order by 1,2
@@ -32,7 +30,6 @@ order by 1,2
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
@@ -43,7 +40,6 @@ order by TotalDeathCount desc
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -53,6 +49,5 @@ order by PercentPopulationInfected desc
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
